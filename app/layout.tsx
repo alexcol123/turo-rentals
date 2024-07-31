e063a9fe00242161f2f0e7194bc488f2172eae26
Henry Munoz <alexcol123@yahoo.com>
2024-07-31 09:58:51 -0400

    chore: Update metadata title and openGraph properties in layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,8 +10,23 @@ import {
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Turo | Find your perfect car",
+  title: "TuroRentals | Find your perfect car",
   description: "Find your perfect car on Turo, the world's largest car sharing marketplace.",
+  openGraph: {
+    type: "website",
+    locale: "en_US",
+    url: "https://turo-rentals.vercel.app/",
+    title: "TuroRentals | Find your perfect car",
+    description: "Find your perfect car on Turo, the world's largest car sharing marketplace.",
+    images: [
+      {
+        url: "https://turo-rentals.vercel.app/corvette.jpg ",
+        width: 1200,
+        height: 630,
+        alt: "TuroRentals | Find your perfect car",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
@@ -22,7 +37,7 @@ export default function RootLayout({
   return (
     <html lang='en' suppressHydrationWarning>
       <body className={inter.className}>
-      <ClerkProvider >
+        <ClerkProvider >
           <Providers>
             <Navbar />
             <main className="container py-10">{children}</main>
